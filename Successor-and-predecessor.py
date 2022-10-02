ed9412a682ea5176d59bef2f2d3dfd0dae8c779a
KalaniMariano <kalanimariano@gmail.com>
2022-10-02 20:33:48 -0300

    .

---
+++ Successor-and-predecessor.py
@@ -0,0 +1,8 @@
+def successorandpredecessor():
+    print ("Choose a number to know your successor and predecessor")
+    a = int(input("n1: "))
+    s = a + 1
+    p = a - 1
+    print (f"Sucessor: {s}\nPredecessor: {p}")
+    
+successorandpredecessor()
---
+++ Sum-of-two-numbers.py
@@ -0,0 +1,7 @@
+def soma():
+    a = int(input("Type a number: "))
+    b = int(input("Type a number: "))
+    s = a + b
+    print (f"Their sum is: {s}")
+
+soma()
