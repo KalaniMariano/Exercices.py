228d4303db8f8197ec8bf65e31a47bffa93e6b36
KalaniMariano <kalanimariano@gmail.com>
2022-10-03 18:08:57 -0300

    006

---
+++ 006-Multiplication-table.py
@@ -0,0 +1,8 @@
+def multiplication():
+    n = int(input("Choose a number to know your multiplication table: "))
+    while n < 0 or n > 9:
+        print ("The number must be between 1 and 9")
+        n = int (input("Try again: "))
+    for i in range (1,11):
+        print (f"{n} * {i} = {n * i}")
+multiplication()
