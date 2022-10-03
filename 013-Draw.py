5cc572e18a5db3b924e0b484163f1cc30efe4a55
KalaniMariano <kalanimariano@gmail.com>
2022-10-03 18:09:36 -0300

    013

---
+++ 013-Draw.py
@@ -0,0 +1,11 @@
+import random
+
+numberstudants = int(input("How many studants do you want to draw? "))
+namestudants = []
+while numberstudants <= 1:
+    print ("The number of students drawn must be greater than 1")
+    numberstudants = int(input("Try again: "))
+for i in range(numberstudants):
+    names = namestudants.append(input("Name of the student: "))
+
+print ("The draw is:", random.choice(namestudants))
