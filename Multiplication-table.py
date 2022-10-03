1f532f6183a68aeadeb8b084877cd1f3c3c1e481
KalaniMariano <kalanimariano@gmail.com>
2022-10-02 21:08:40 -0300

    Create Multiplication-table.py

---
+++ Multiplication-table.py
@@ -0,0 +1,10 @@
+def multiplication():
+    list[1,2,3,4,5]
+    print (list)
+    n = int(input("Choose a number to know your multiplication table: "))
+    while n < 0 or n > 9:
+        print ("The number must be between 1 and 9")
+        n = int (input("Try again: "))
+    for i in range (11):
+        print (f"{n} * {i} = {n * i}")
+multiplication()
