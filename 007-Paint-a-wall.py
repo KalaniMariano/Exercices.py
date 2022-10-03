3504bcf8a36fe47356d2d71a0b1afd3ad7c0fba4
KalaniMariano <kalanimariano@gmail.com>
2022-10-03 18:09:02 -0300

    007

---
+++ 007-Paint-a-wall.py
@@ -0,0 +1,4 @@
+w = float(input("Width of the wall: "))
+h = float(input("Height: "))
+m2 = (w * h)/2
+print (f"You will need {round(m2,2)} liters of ink to paint your wall knowing that your ink paints 2m²/L")
