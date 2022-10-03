c4af4e02093b3bac4f125c804c873e0105408b65
KalaniMariano <kalanimariano@gmail.com>
2022-10-03 18:05:50 -0300

    Update Decimal-places.py

--- Decimal-places.py
+++ Decimal-places.py
@@ -1,3 +1,6 @@
 n = input("Type a number between 1 an 9999: ")
-n = []
-print (n)
+while len(n) > 4:
+    n = input("Try again: ")
+decimal = ["Thousand", "Hundred", "Ten", "Unity"]
+for i in range (len(n)):
+    print (f"{decimal[(i-len(n))]}: {n[i]}")
