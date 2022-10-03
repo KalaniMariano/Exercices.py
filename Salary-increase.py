c5238f198ab7e581f5ae358bc1dff4832b47923b
KalaniMariano <kalanimariano@gmail.com>
2022-10-02 22:23:59 -0300

    Create Salary-increase.py

---
+++ Salary-increase.py
@@ -0,0 +1,11 @@
+salary = float(input("Salary: "))
+while salary <= 0:
+    print ("The salary must be greater than 0")
+    salary = float(input("Try again: "))
+increase = float(input("How many % do you want to increase? "))
+while increase < 0 or increase > 100:
+    print ("The increase must be between 1 and 100%")
+    increase = float(input("Try again: "))
+x = (salary/100)*increase
+salary = salary + x
+print ("The new salary is: ", round(salary,2))
