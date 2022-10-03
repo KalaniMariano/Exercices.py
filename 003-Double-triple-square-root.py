9d93b3d39b3d78a68c5b64859b0b0dd2b9321027
KalaniMariano <kalanimariano@gmail.com>
2022-10-03 18:08:44 -0300

    003

---
+++ 003-Double-triple-square-root.py
@@ -0,0 +1,10 @@
+import math
+def main():
+    n = float(input("Choose a number: "))
+    d = n * 2
+    t = n * 3
+    s = math.sqrt(n)
+    print ("Double: ",d)
+    print ("Triple: ",t)
+    print ("Square root: ",s)
+main()
