f90e1f87f4ece04149b0b4c7d7813d0917dc6013
KalaniMariano <kalanimariano@gmail.com>
2022-10-03 18:35:39 -0300

    004

--- 004-Successor-and-predecessor.py
+++ 004-Successor-and-predecessor.py
@@ -1,8 +1,6 @@
 def successorandpredecessor():
     print ("Choose a number to know your successor and predecessor")
     a = int(input("n1: "))
-    s = a + 1
-    p = a - 1
-    print (f"Sucessor: {s}\nPredecessor: {p}")
+    print (f"Sucessor: {a+1}\nPredecessor: {a-1}")
     
 successorandpredecessor()
