e04a47fd45dfaca0473bebae57593d2a239730a4
KalaniMariano <kalanimariano@gmail.com>
2022-10-02 21:26:42 -0300

    Update Multiplication-table.py

--- Multiplication-table.py
+++ Multiplication-table.py
@@ -1,6 +1,4 @@
 def multiplication():
-    list[1,2,3,4,5]
-    print (list)
     n = int(input("Choose a number to know your multiplication table: "))
     while n < 0 or n > 9:
         print ("The number must be between 1 and 9")
