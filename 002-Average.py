bfe98fc59ca2703c1a600b9dfab420bb37deb3d0
KalaniMariano <kalanimariano@gmail.com>
2022-10-03 18:33:53 -0300

    002

--- 002-Average.py
+++ 002-Average.py
@@ -1,6 +1,5 @@
 def average():
     n1 = float(input("Number 1/2: "))
     n2 = float(input("Number 2/2: "))
-    a = (n1 + n2)/2
-    print ("Average: ", a)
+    print ("Average: ",(n1 + n2)/2)
 average()
