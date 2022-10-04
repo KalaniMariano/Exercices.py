a4be022fa3d8840ada6118b0c18c6a556fdfead7
KalaniMariano <kalanimariano@gmail.com>
2022-10-03 21:10:23 -0300

    026

--- 026-Salary-increase-with-if.py
+++ 026-Salary-increase-with-if.py
@@ -2,11 +2,11 @@ salary = float(input("Enter a salary for increase: "))
 while salary <= 0:
     print ("The salary must be greater than 0")
     salary = float(input("Try again: "))
-if salary >= 1250:
+if salary > 1250:
     x = (salary/100)*10
     salary += x
-    print (f"The increase was 10%: R${salary}")
+    print (f"The increase was 10%: R${round(salary,2)}")
 else:
     x = (salary/100)*15
     salary += x
-    print (f"The increase was 15%: R${salary}")
+    print (f"The increase was 15%: R${round(salary,2)}")
