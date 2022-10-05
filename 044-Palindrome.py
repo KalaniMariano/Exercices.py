14f4ab840ad243be77ff51def48673f3d9339dc3
KalaniMariano <kalanimariano@gmail.com>
2022-10-05 10:36:51 -0300

    044

--- 044-Palindrome.py
+++ 044-Palindrome.py
@@ -3,9 +3,12 @@ phrase = str(input("Type a sentence: "))
 length_phrase = len(phrase)
 
 inverted_phrase = phrase[length_phrase::-1]
+list_phrase = phrase.split()
+join_list_phrase = ''.join(list_phrase)
+list_inverted_phrase = inverted_phrase.split()
+join_list_inverted_phrase = ''.join(list_inverted_phrase)
 
-if inverted_phrase == phrase:
-    print ("Your sentence is a palindrome :)\n")
+if join_list_inverted_phrase == join_list_phrase:
+    print ("Your sentence is a palindrome :)")
 else:
-    print ("Your sentence is NOT a palindrome :(\n")
-print (inverted_phrase)
+    print ("Your sentence is NOT a palindrome :(")
