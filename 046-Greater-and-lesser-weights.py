17ef4b1f22bcaef38c39161ce71f3d1b3c0cb4d1
KalaniMariano <kalanimariano@gmail.com>
2022-10-05 13:54:45 -0300

    046

--- 046-Greater-and-lesser-weights.py
+++ 046-Greater-and-lesser-weights.py
@@ -4,34 +4,32 @@ show which was the highest and which was the lowest weight read'''
 print("Enter the weight of 5 people in kg.\n")
 
 prompt = ""
-highest_weight = 0
-lowest_weight = 0
-dif_highest_weight = 0
-dif_lowest_weight = 0
 
-for i in range(5):
-    print (f"{i+1}> ", end="")
+high = 0
+low = 0
+
+for p in range(1,6):
+    print (f"{p} > ", end='')
     while True:
         try:
-            new_weight = float(input(prompt))
+            new= float(input(prompt))
         except:
             print("Only numbers, try again...")
             continue
-        if new_weight <= 0 or new_weight >= 595:
+        if new <= 0 or new >= 595:
             print("The weight must be between 0 and 595.")
-            prompt = "> "
+            prompt = ""
         else:
             break
-    if True:
-        if lowest_weight == 0:
-            lowest_weight = (new_weight - dif_highest_weight)
-        elif new_weight > highest_weight:
-            dif_highest_weight = new_weight - highest_weight
-            highest_weight += dif_highest_weight
-        elif new_weight < lowest_weight:
-            dif_lowest_weight = lowest_weight - new_weight
-            lowest_weight -= dif_lowest_weight
+    if p == 1:
+        high = new
+        low = new
+    else:
+        if new > high:
+            high = new
+        if new < low:
+            low = new
 
-print (f"\nThe highest weight was {highest_weight} and the lowest was {round(lowest_weight,2)}")
+print (f"\nThe highest weight was {round(high,2)} and the lowest was {round(low,2)}")
 
     
