087f6b461e781a3783c2ee9c84da6c8e842a5127
Kalani Mariano <kalanimariano@gmail.com>
2022-10-08 13:11:00 -0300

    058

--- 058-Break-multiplication-table.py
+++ 058-Break-multiplication-table.py
@@ -1,12 +1,13 @@
 '''multiplication table with break for negative number'''
-
-n = int(input("Choose a number to know your multiplication table: "))
-i =0
+i = 0
 while True:
-    while n > 9:
-        print ("The number must be less than 9")
-        n = int(input("Try again: "))
-    if n < 0 or i == 10:
+    print ("Type a negative number to stop.")
+    n = int(input("Choose a number to know your multiplication table: "))
+    if n < 0:
         break
     for i in range (1,11):
         print (f"{n} * {i} = {n * i}")
+        if i == 10:
+            print ("=-="*10)
+            break
+print ("Finished")
