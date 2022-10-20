19b9e86d82aeb0e0b272cfa28caf194c645189b8
Kalani Mariano <kalanimariano@gmail.com>
2022-10-20 19:49:37 -0300

    090

---
+++ 090-Functions-greater.py
@@ -0,0 +1,39 @@
+'''Write a program that has a function called greater(), which takes several parameters with integer values.
+
+Your program has to analyze all the values ​​and say which one is the biggest.'''
+from time import sleep
+
+def greater(lst):
+    greater_num = 0
+    for i in range(len(lst)):
+        if i == 0 or lst[i] > greater_num:
+            greater_num = lst[i]
+    print('=-='*20)
+    print('Analyzing past values', end='')
+    for i in range(3):
+        sleep(0.5)
+        print('.',end='')
+    print()
+    for i in lst:
+        print(i,end=' ')
+    if len(lst) == 1:
+       print(f'- Was informed {len(lst)} value.')
+    else:
+        print(f'- Were informed {len(lst)} values.')
+    print(f'The highest number was {greater_num}')
+    print('=-='*20)
+
+
+nums_list = []
+while True:
+    nums = int(input('How many numbers do you want to write? '))
+    for i in range(nums):
+        n = int(input(f'Number {i+1}: '))
+        nums_list.append(n)
+    greater(nums_list)
+    nums_list.clear()
+    choice = str(input('Type "n" to end the program, anything else to continue: '))
+    if choice in 'Nn':
+        print('\33[32mGood bye...\33[m')
+        break
+    print('-'*60)
