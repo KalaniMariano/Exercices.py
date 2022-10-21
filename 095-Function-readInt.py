a58c8eb08fc6826bcf58e16d954c635a0cdb7a5c
Kalani Mariano <kalanimariano@gmail.com>
2022-10-21 02:18:27 -0300

    095

---
+++ 095-Function-readInt.py
@@ -0,0 +1,16 @@
+'''Create a program that has a function called readInt(), 
+which will work similarly to Python's input() function, 
+only validating to accept only a numeric value.
+Ex: readInt('Enter an n')'''
+def readInt(number):
+    while True:
+        print(number, end='')
+        typed_number = str(input())
+        if typed_number.isnumeric():
+            return typed_number
+            break
+        else:
+            print("\033[031mOops, looks like you didn't typed an integer.\033[m")
+
+n = readInt('Enter an integer: ')
+print(f'You just typed the number {n}')
