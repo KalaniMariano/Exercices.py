aaa95b5d24217da742aa5be5e65cacae54293260
Kalani Mariano <kalanimariano@gmail.com>
2022-10-20 23:53:54 -0300

    089

--- 089-Functions-counter.py
+++ 089-Functions-counter.py
@@ -8,36 +8,32 @@ B) From 10 to 0, from 2 to 2.
 C) A custom count.'''
 from time import sleep
 
-def counter(* num):
+def counter(i,f,p):
     print('=-='*20)
-    print('Count from 1 to 10, from 1 to 1')
-    for i in range(1,11):
-        print(i, end=' ')
-        sleep(0.5)
-    print()
-    print('=-='*20)
-    print('Count from 10 to 0, from 2 to 2')
-    for i in range(10,-2,-2):
-        print(i,end=' ')
-        sleep(0.5)
-    print()
-    print('=-='*20)
-    print("Now it's your turn to customize the count")
-    a = int(input('Start: '))
-    b = int(input('End: '))
-    c = int(input('Step: '))
-    print()
-    if c == 0:
-        c +=1
-    if a > b and c > 0:
-        c *= -1
-    print(f'Count from {a} to {b}, from {c} to {c}')
-    if c < 0:
-        b -= 1
+    if p == 0: #if step == 0
+        p += 1 #add 1 to step
+
+    if i > f and p > 0: #if the start is greater than the end and step is > 0, the step needs to be decreasing
+        p *= -1         #so multiply the step by -1
+
+    print(f'Count from {i} to {f}, from {p} to {p}')
+
+    if p < 0:   #this "if" is for the "for" show the number that the user entered for the "end"
+        f -= 1
     else:
-        b += 1
-    for i in range(a,b,c):
-        print(i,end=' ')
-        sleep(0.5)
+        f += 1
+
+    for x in range(i,f,p):
+        print(x,end=' ')
+        sleep(0.2)
         
-counter()
+    print('\33[32mTHE END...\33[m')
+
+
+counter(0,10,1)
+counter(10,0,1)
+print("Now it's your turn to customize the count")
+a = int(input('Start: '))
+b = int(input('End: '))
+c = int(input('Step: '))
+counter(a,b,c)
